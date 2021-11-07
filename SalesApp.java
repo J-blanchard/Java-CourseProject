2129b70436d9e8190e2803768632c80ceeb62ac3
J-blanchard <Javinblanchard@outlook.com>
2021-11-07 18:52:22 -0500

    Added Update feature

--- SalesApp.java
+++ SalesApp.java
@@ -9,7 +9,7 @@ public class SalesApp {
 	
 	private final static Logger LOGGER = Logger.getLogger(MyLogger.class.getName());
 	public static HashMap<String, Object> map = new HashMap<String, Object>();
-	public static ArrayList<ResultSet> tranIDList = new ArrayList<ResultSet>();
+	public static ArrayList<Transactions> tranIDList = new ArrayList<Transactions>();
 	
 	public static void main(String[] args) throws SecurityException, IOException, SQLException{
 			
@@ -18,7 +18,7 @@ public class SalesApp {
 		// LOGGER.info("Logger Name: "+LOGGER.getName());
 		
 	        try{
-	        	System.out.println("Welcome to the sales application!\n");
+	        	System.out.println("Welcome to the sales application!");
 	    		//calls the navigation method
 	    		mainMenu();
 	    		// LOGGER.info("info msg");
@@ -31,13 +31,13 @@ public class SalesApp {
 	
 	//-----------------------------------------------------------MAIN MENU----------------------------------------------------------------------//
 	public static void mainMenu() throws SQLException {
-		
+		System.out.println("\n|[MAIN MENU]|\n");
 		System.out.println("Please select what you would like to do:\n");
 		System.out.println("[1: Purchase an item.]");
 		System.out.println("[2: Add a new item.]");
-		System.out.println("[3: Remove an item.]");
-		System.out.println("[4: View transaction report.]");
-		System.out.println("[5: View inventory report.]");
+		System.out.println("[3: Update an item.]");
+		System.out.println("[4: Remove an item.]");
+		System.out.println("[5: View transaction report.]");
 		System.out.println("[6: Exit application.]");
 		Scanner input = new Scanner(System.in);
 		int selection = input.nextInt();
@@ -48,14 +48,14 @@ public class SalesApp {
 			addItem();
 		}
 		if (selection == 3) {
-			removeItem();
+			UpdateItem();
 		}
 		if (selection == 4) {
+			removeItem();
+		}
+		if (selection == 5) {
 			showTransactionReport();
 		}
-		// if (selection == 5) {
-		// 	showInventoryReport();
-		// }
 		if (selection == 6) {
 			System.out.println("Thank you, enjoy your day!");
 		}
@@ -178,6 +178,93 @@ public class SalesApp {
 		
 		
 	}
+	//======================================UPDATES ITEM=========================================================//
+	public static void UpdateItem() throws SQLException {
+
+		System.out.println("Please enter the Item Id of the Product you would like to update: ");
+		Scanner input = new Scanner(System.in);
+		String Id = input.nextLine().toUpperCase();
+		//ADD INPUT VALIDATION. Check to see if ID Exists
+		System.out.println("What would you like to update?");
+		System.out.println("[1: All.]");
+		System.out.println("[2: Item ID.]");
+		System.out.println("[3: Item Name.]");
+		System.out.println("[4: Item Price.]");
+		System.out.println("[5: Item Quantity.]");
+		int UpdateSelection = input.nextInt();
+
+		//All
+		if (UpdateSelection == 1){
+
+		System.out.println("Please enter the desired item ID: ");
+		String newId = input.nextLine();
+
+		System.out.println("Please enter the name of the item: ");
+			String ItemName = input.nextLine();
+
+		System.out.println("Please enter the price of the item: ");
+			Double price = input.nextDouble();
+
+		System.out.println("Please enter the quantity of the item in stock: ");
+			int quantity = input.nextInt();
+
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courseproject", "root", "root");
+			Statement stmt = connection.createStatement();
+			String query = "UPDATE Product SET ProductId = "+ newId +", ProductName = \""+ ItemName + "\", Productprice = "+ price +" ProductQuantity = " + quantity + " WHERE ProductId = \"" + Id + "\" ;";
+			stmt.executeUpdate(query);
+		}
+		//Item ID
+		else if (UpdateSelection == 2){
+			System.out.println("Please enter the desired item ID: ");
+				Scanner scanner = new Scanner(System.in);
+				String newId = scanner.nextLine();
+
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courseproject", "root", "root");
+				Statement stmt = connection.createStatement();
+				String query = "UPDATE Product SET ProductId = " + newId + " WHERE ProductId = \"" + Id + "\" ;";
+				stmt.executeUpdate(query);
+		}
+		//Item name
+		else if (UpdateSelection == 3){
+			System.out.println("Please enter the name of the item: ");
+			Scanner scanner = new Scanner(System.in);
+				String ItemName = scanner.nextLine();
+
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courseproject", "root", "root");
+				Statement stmt = connection.createStatement();
+				String query = "UPDATE Product SET ProductName = \"" + ItemName + "\" WHERE ProductId = \"" + Id + "\" ;";
+				stmt.executeUpdate(query);
+		}
+		//Item Price
+		else if (UpdateSelection == 4){
+			System.out.println("Please enter the price of the item: ");
+			Scanner scanner = new Scanner(System.in);
+				Double price = scanner.nextDouble();
+
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courseproject", "root", "root");
+				Statement stmt = connection.createStatement();
+				String query = "UPDATE Product SET Productprice = " + price + " WHERE ProductId = \"" + Id + "\" ;";
+				stmt.executeUpdate(query);
+		}
+		//Item Quantity
+		else if (UpdateSelection == 5){
+			System.out.println("Please enter the quantity of the item in stock: ");
+			Scanner scanner = new Scanner(System.in);
+				int quantity = scanner.nextInt();
+
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courseproject", "root", "root");
+				Statement stmt = connection.createStatement();
+				String query = "UPDATE Product SET ProductQuantity = " + quantity + " WHERE ProductId = \"" + Id + "\" ;";
+				stmt.executeUpdate(query);
+		}
+		else{
+			System.out.println("Selection was invalid. Please choose a selection between 1-5...");
+			UpdateItem();
+		}
+		System.out.println("Item was successfully updated!");
+		mainMenu();
+
+	}
 
 	//===================================================REMOVES ITEM-============================================//
 	public static void removeItem() throws SQLException {
@@ -203,8 +290,13 @@ public class SalesApp {
 		String query = "Select * from transactions";
 		ResultSet rs = stmt.executeQuery(query);
 		while (rs.next()){
+			Transactions trans = new Transactions();
+			trans.setID(rs.getInt(1));
+			trans.setName(rs.getString(2));
+			trans.setPrice(rs.getDouble(3));
+			trans.setQuantity(rs.getInt(4));
 
-			tranIDList.add(rs);
+			tranIDList.add(trans);
 		}
 	}
 	public static void CreateTransaction(String productSold, double price, int AmountPurchased) throws SQLException{
@@ -218,15 +310,21 @@ public class SalesApp {
 
 	public static void showTransactionReport() throws SQLException{
 		populateTransactionList();
-		System.out.println("|  Transaction ID  |  Product  |  Price  |  Quantity Sold |");
-		System.out.println("|__________________|___________|_________|________________|");
-
-		for (ResultSet rs : tranIDList) {
-			rs.next();
-			  System.out.println(rs.getInt(1)); 
-			  //https://www.tutorialguruji.com/java/java-sql-sqlexception-after-end-of-result-set-error-in-mysql/
+		
+		System.out.println("\n|[TRANSACTION REPORT]|");
+		System.out.println("______________________");
+		for (Transactions tran : tranIDList) {
+			
+			System.out.println("|[ Transaction ID: "+ tran.getId() + " ]|[ Product: " + tran.getName() + " ]|[ Price: $" + tran.getPrice()+ " ]|[ Quantity Sold: " + tran.getQuantity() + " ]|");
 		}
-		// System.out.println("|    "+ (i +1) + "    |  Product  |  Price  |  Quantity Sold |");
+		System.out.print("\nTo go back to the main menu enter any character...");
+		Scanner input = new Scanner(System.in);
+		String usrInput = input.nextLine();
+		
+		if (usrInput != ""){
+			mainMenu();
+		}
+		
 	}
 
 	//TAKES IN id and returns productName
@@ -240,7 +338,13 @@ public class SalesApp {
 			while (rs.next()){
 				String itemName = rs.getString(2);
 		connection.close();
-				return itemName;
+		if(getProductQuantity(Selectedid) > 0){
+			return itemName;
+		}
+		else{
+			return "N/A";
+		}
+				
 			}
 			
 		return "Empty";
@@ -275,5 +379,41 @@ public class SalesApp {
 	
 
 }
+class Transactions{
+	private int Id;
+	private String productName;
+	private int quantity;
+	private double price;
 
+	public Transactions(){
+
+	}
+	//setters
+	public void setID(int _id){
+		this.Id = _id;
+	}
+	public void setName(String newName){
+		this.productName = newName;
+	}
+	public void setQuantity(int _quantity){
+		this.quantity = _quantity;
+	}
+	public void setPrice(Double _Price){
+		this.price = _Price;
+	}
+	//getters
+	public int getId(){
+		return Id;
+	}
+	public String getName(){
+		return productName;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
+	public double getPrice(){
+		return price;
+	}
+
+}
 
